2ce0388e21bdfb289f691ed0b3de557cc0251616
Gladwin-32 <santhosh@lyfskills.co>
2026-05-22 21:12:33 +0530

    updated sitenpa

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -14,19 +14,20 @@ const routes = [
 ];
 
 function generateSitemap() {
+  const currentDate = new Date().toISOString().split("T")[0]; // YYYY-MM-DD
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${routes
-    .map((route) => {
-      return `
+${routes
+  .map((route) => {
+    return `
   <url>
     <loc>${BASE_URL}${route.url}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>${route.changefreq}</changefreq>
-    <priority>${route.priority}</priority>
+    <lastmod>${currentDate}</lastmod>
   </url>`;
-    })
-    .join("")}
+  })
+  .join("")}
+
 </urlset>
 `;
 
